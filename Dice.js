0699afae80ea2a23f08068f17c8ac1cb1d003df9
Pratikshagalhe <pratikshagalhe@gmail.com>
2024-06-04 14:56:27 +0530

    Done

--- Dice.js
+++ Dice.js
@@ -1,6 +1,5 @@
-
 function roll(){
-    setTimeout(function(){
+    
         var randomNo1 = Math.floor(Math.random()*6)+1;
         var randomNo2 = Math.floor(Math.random()*6)+1;
 
@@ -9,12 +8,15 @@ function roll(){
 
         if(randomNo1>randomNo2){
           document.querySelector(".win").innerHTML="Player 1 wins !!";
+          document.querySelector(".win").style.color= "green";
         }
         else if(randomNo1==randomNo2){
-            document.querySelector(".win").innerHTML="Draw!";
+            document.querySelector(".win").innerHTML="Draw!!";
+            document.querySelector(".win").style.color= "green";
         }
         else{
             document.querySelector(".win").innerHTML="Player 2 wins !!";
+            document.querySelector(".win").style.color= "green";
         }
-    },2000);
+   
 }
