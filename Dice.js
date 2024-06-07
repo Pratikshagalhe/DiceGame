0dcfb50f92dafec17e1523cb2e5e62bad02669e4
Pratikshagalhe <pratikshagalhe@gmail.com>
2024-06-07 13:07:33 +0530

    done

--- Dice.js
+++ Dice.js
@@ -3,8 +3,8 @@ function roll(){
         var randomNo1 = Math.floor(Math.random()*6)+1;
         var randomNo2 = Math.floor(Math.random()*6)+1;
 
-        document.querySelector(".img1").setAttribute("src","/Html5AndCss3/DiceGame/imageFolder/d"+randomNo1+".jpg");
-        document.querySelector(".img2").setAttribute("src","/Html5AndCss3/DiceGame/imageFolder/d"+randomNo2+".jpg");
+        document.querySelector(".img1").setAttribute("src","/imageFolder/d"+randomNo1+".jpg");
+        document.querySelector(".img2").setAttribute("src","/imageFolder/d"+randomNo2+".jpg");
 
         if(randomNo1>randomNo2){
           document.querySelector(".win").innerHTML="Player 1 wins !!";
